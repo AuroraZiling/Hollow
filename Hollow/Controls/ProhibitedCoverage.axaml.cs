55a19b74537c7d18cccaa94dd6b4a6c7c77988c5
Mccree Lee <auroraziling@gmail.com>
2024-07-08 17:51:24 +0800

    feat: prohibited coverage

---
+++ Hollow/Controls/ProhibitedCoverage.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Hollow.Controls;
+
+public class ProhibitedCoverage : UserControl
+{
+    public static readonly StyledProperty<string> TextProperty =
+        AvaloniaProperty.Register<ProhibitedCoverage, string>(nameof(Text));
+
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+}

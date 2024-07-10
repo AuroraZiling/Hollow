ba9af1233b1c9ed501610f86aac476e66c792416
Mccree Lee <auroraziling@gmail.com>
2024-07-10 11:41:02 +0800

    feat: app icon

--- build.ps1
+++ build.ps1
@@ -6,13 +6,13 @@ $ErrorActionPreference = "Stop";
 
 Write-Output "Start building withRuntime...";
 
-dotnet publish Hollow/Hollow.csproj -o "build/$Version/withRuntime" -p:EnableCompressionInSingleFile=true -p:PublishSingleFile=true -p:Platform=$Architecture -p:PublishReadyToRun=true -p:SelfContained=true -p:AssemblyVersion=$Version -p:Configuration=Release;
+dotnet publish Hollow/Hollow.csproj -o "build/$Version/withRuntime" -p:EnableCompressionInSingleFile=true -p:PublishSingleFile=true -p:Platform=win-x64 -p:SelfContained=true -p:AssemblyVersion=$Version -p:Configuration=Release;
 
 Rename-Item -Path "build/$Version/withRuntime/Hollow.exe" -NewName "Hollow_withRuntime.exe"
 
 Write-Output "Start building withoutRuntime...";
 
-dotnet publish Hollow/Hollow.csproj -o "build/$Version/withoutRuntime" -p:PublishSingleFile=true -p:Platform=$Architecture -p:PublishReadyToRun=true -p:SelfContained=false -p:AssemblyVersion=$Version -p:Configuration=Release;
+dotnet publish Hollow/Hollow.csproj -o "build/$Version/withoutRuntime" -p:Platform=win-x64 -p:PublishReadyToRun=true -p:SelfContained=false -p:AssemblyVersion=$Version -p:Configuration=Release;
 
 Write-Output "Build Finished";
 

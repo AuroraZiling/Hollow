b7510fbc2576b7b4a3b379d1a8e34cf463e045a9
Mccree Lee <auroraziling@gmail.com>
2024-07-25 19:42:42 +0800

    perf: reduce generated files

--- Hollow.Windows/Program.cs
+++ Hollow.Windows/Program.cs
@@ -9,9 +9,6 @@ namespace Hollow.Windows;
 
 public static class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
@@ -35,10 +32,8 @@ public static class Program
         }
     }
 
-    // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
-            .LogToTrace()
-            .WithInterFont();
+            .LogToTrace();
 }
